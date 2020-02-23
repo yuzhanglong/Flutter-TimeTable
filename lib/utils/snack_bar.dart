bd69b8558235df49472d6efac5c8f189fd245050
yuzhanglong <yuzl1123@163.com>
2020-02-23 17:11:52 +0800

    封装snackBar

---
+++ lib/utils/snack_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Snack{
+  static Widget error(String text, {int duration = 1}){
+    return SnackBar(
+      content: Text(text),
+      backgroundColor: Colors.red,
+      duration: Duration(seconds: duration),
+    );
+  }
+
+  static Widget success(String text, {int duration = 1}){
+    return SnackBar(
+      content: Text(text),
+      backgroundColor: Colors.green,
+      duration: Duration(seconds: duration),
+    );
+  }
+
+  static Widget info(String text, {int duration = 1}){
+    return SnackBar(
+      content: Text(text),
+      backgroundColor: Colors.grey,
+      duration: Duration(seconds: duration),
+    );
+  }
+
+  static Widget waring(String text, {int duration = 1}){
+    return SnackBar(
+      content: Text(text),
+      backgroundColor: Colors.yellow,
+      duration: Duration(seconds: duration),
+    );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
